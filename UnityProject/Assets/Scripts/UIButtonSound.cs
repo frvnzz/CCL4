c45affcd8d0740960b0aaf4d9fc1be3eb90d47ef
SelinaHacker <cc231072@fhstp.ac.at>
2025-06-17 22:38:38 +0200

    main menu sounds

---
+++ UnityProject/Assets/Scripts/UIButtonSound.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class UIButtonSound : MonoBehaviour
+{
+    public GameObject soundSource;
+    public GameObject menuCanvas;
+    public bool isQuitButton = false; // Flag für Quit
+
+    public void onClick()
+    {
+        if (soundSource != null)
+            AkSoundEngine.PostEvent("Play_click_sound", soundSource);
+        else
+            AkSoundEngine.PostEvent("Play_click_sound", gameObject);
+
+        StartCoroutine(DoAfterSound());
+    }
+
+    private IEnumerator DoAfterSound()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if (isQuitButton)
+        {
+            Application.Quit();
+            Debug.Log("Quits game");
+        }
+        else if (menuCanvas != null)
+        {
+            menuCanvas.GetComponent<SceneTransition>().SwitchCanvas();
+        }
+    }
+}
